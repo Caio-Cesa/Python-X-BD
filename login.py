2a22375157281ba5af1cca6e3a166e732d44f723
Caio-Cesa <caiothebrazilian@gmail.com>
2025-11-10 21:14:29 -0300

    extensao
    atualização pra extensão única

--- login.py
+++ login.py
@@ -56,7 +56,7 @@ frame = ttk.Frame(janela, padding=20)
 frame.place(relx=0.5, rely=0.5, anchor="center")
 
 ttk.Label(frame, text="Acesso ao Sistema", font=("Segoe UI", 14, "bold")).pack(pady=(0, 15))
-ttk.Label(janela,text="Matrícula: ").pack(anchor="w")
+ttk.Label(frame,text="Matrícula: ").pack(anchor="w")
 entrada_usuario = ttk.Entry(frame, width=30)
 entrada_usuario.pack(pady=5)
 
@@ -64,7 +64,7 @@ ttk.Label(frame,text="Senha: ").pack(anchor="w")
 entrada_senha = ttk.Entry(frame, width=30, show="*")
 entrada_senha.pack(pady=5)
 
-tk.Button(janela,text="Entrar",command=verificar_login).pack(pady=15)
+tk.Button(frame,text="Entrar",command=verificar_login).pack(pady=15)
 entrada_usuario.focus()
 
 janela.mainloop()
